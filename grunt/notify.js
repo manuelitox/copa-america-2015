bbee77766e7359be955dc189bc1a10067017b23d
Manuel Gonzalez <manuel2788@gmail.com>
2015-06-08 08:57:32 -0430

    grunt-notify.js: configuration updates

--- grunt/notify.js
+++ grunt/notify.js
@@ -3,8 +3,8 @@ module.exports = {
   notify_hooks: {
     options: {
       enabled: true,
-      max_jshint_notifications: 5, // maximum number of notifications from jshint output
-      title: "Project Name", // defaults to the name in package.json, or will use project directory's name
+      max_jshint_notifications: 8, // maximum number of notifications from jshint output
+      title: "Copa America 2015", // defaults to the name in package.json, or will use project directory's name
       success: false, // whether successful grunt executions should be notified automatically
       duration: 3 // the duration of notification in seconds, for `notify-send only
     }
