6bff9416ee9f079e08bf6e9b6817c75df71952e0
Manuel Gonzalez <manuel2788@gmail.com>
2015-06-10 16:54:21 -0430

    starting final phase develop

---
+++ app/assets/reactjs/calendars/finals/base.jsx
@@ -0,0 +1,16 @@
+var React;
+
+React = require('react');
+
+APP = React.createClass({	
+	getInitialState: function() {
+		return { 
+
+		};
+	}, 	
+	render: function() {
+		return (
+
+		);
+	}
+});
