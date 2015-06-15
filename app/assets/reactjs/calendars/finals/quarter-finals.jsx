13a2cb8f7c116f5c2b0f78db47cf0c6a1c54c01d
Manuel Gonzalez <manuel2788@gmail.com>
2015-06-15 18:54:48 -0430

    Get group winners (first and second positions) and third places.

--- app/assets/reactjs/calendars/finals/quarter-finals.jsx
+++ app/assets/reactjs/calendars/finals/quarter-finals.jsx
@@ -9,8 +9,44 @@ module.exports = React.createClass({
 		return ({  
 			title: 'Cuartos de Final'
 		});
-	},	 	
+	},
+	teams: function() {
+		var groups, winners = [], thirdPlace = {};
+		groups = JSON.parse(localStorage.groups);
+		winners.a = this.getWinners(groups.a);
+		winners.b = this.getWinners(groups.b);
+		winners.c = this.getWinners(groups.c); 
+		thirdPlace.a = this.getThirdPlace(groups.a);
+		thirdPlace.b = this.getThirdPlace(groups.b);
+		thirdPlace.c = this.getThirdPlace(groups.c);
+		return winners;
+		//return this.getTwoBestThird(thirdPlace);
+	},
+	getWinners: function(groups) {
+		var teams = [];
+		if (groups.length >= 1) {
+			groups[0].map(function (team, i) {
+			 	i <= 1 ? teams.push(team) : null;
+		 	});
+	 	}
+	 	return teams;
+	},
+	getThirdPlace: function(groups) {
+		var teams = [];
+		if (groups.length >= 1) {
+			groups[0].map(function (team, i) {
+				i == 2 ? teams.push(team) : null;
+			});
+		}
+		return teams[0];
+	},
+	// getTwoBestThird: function(thirdPlace) {
+	
+	// },
 	render: function() {
+		console.log(this.teams());
+
+
 		var matchesNode;
 		matchesNode = this.props.matches.map(function (match, index) {
 			return ( <Matches key={ index } match={ match.matches[0] } /> );
