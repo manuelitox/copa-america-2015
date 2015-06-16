6406db850d32dff47fc76f3ba9c9f44e15669b7a
Manuel Gonzalez <manuel2788@gmail.com>
2015-06-16 11:49:29 -0430

    populate the two best third places on quarter finals.

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -220,14 +220,32 @@ module.exports = React.createClass({displayName: "exports",
 		});
 	},
 	populateQuarters: function(matches, winners, twoBestThirdPlaces) {
-		console.log(winners);
+		// console.log(twoBestThirdPlaces);
+		var _this = this;
 		matches.map(function (match, index) {
+			
 			index == 0 ? match.matches[0].local = winners[0] : null;
+			index == 0 ? match.matches[0].visitor = _this.filterThirdPlace(twoBestThirdPlaces, 'B', 'C') : null;
+
 			index == 1 ? match.matches[0].local = winners[1] : null;
+			index == 1 ? match.matches[0].visitor = winners[5] : null;
+			
 			index == 2 ? match.matches[0].local = winners[2] : null;
+			index == 2 ? match.matches[0].visitor = _this.filterThirdPlace(twoBestThirdPlaces, 'A', 'C', _this.filterThirdPlace(twoBestThirdPlaces, 'B', 'C').belong_to) : null;			
+
 			index == 3 ? match.matches[0].local = winners[4] : null;
+			index == 3 ? match.matches[0].visitor = winners[3] : null;
+
 		});
 	},
+	filterThirdPlace: function(thirdPlaces, group, optional, selected) {
+		function filterByGroup(team) {
+			if (team.belong_to != selected) {
+				return team.belong_to == group ? team.belong_to == group : team.belong_to == optional;
+			}
+		};
+		return thirdPlaces.filter(filterByGroup)[0];
+	},		
 	render: function() {
 		
 		// localStorage.length >= 18 ? console.log(ClassifiedTeams.teams()) : null;
